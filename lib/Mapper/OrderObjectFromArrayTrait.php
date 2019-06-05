10486d140fa450d672987efc3d678215a36121fd
marcel <marcel@everycademy.com>
2019-06-05 21:12:28 +0200

    FIXED mapping after unexpected API changes

--- lib/Mapper/OrderObjectFromArrayTrait.php
+++ lib/Mapper/OrderObjectFromArrayTrait.php
@@ -24,22 +24,47 @@ trait OrderObjectFromArrayTrait
     {
         $object = new Order();
         $object->referenceId = $orderResponse['uuid'];
-        $object->orderId = $orderResponse['order_id'];
+
+        if (array_key_exists('order_id', $orderResponse)) {
+            $object->orderId = $orderResponse['order_id'];
+        }
+
+        if (array_key_exists('external_code', $orderResponse)) {
+            $object->orderId = $orderResponse['external_code'];
+        }
+
         $object->state = $orderResponse['state'];
 
         $object->bankAccount = new BankAccount($orderResponse['bank_account']['iban'], $orderResponse['bank_account']['bic']);
 
         $companyAddress = new Address();
-        $companyAddress->street = $orderResponse['debtor_company']['address_street'];
-        $companyAddress->houseNumber = $orderResponse['debtor_company']['address_house_number'];
-        $companyAddress->city = $orderResponse['debtor_company']['address_city'];
-        $companyAddress->postalCode = $orderResponse['debtor_company']['address_postal_code'];
-        $companyAddress->countryCode = $orderResponse['debtor_company']['address_country'];
+
+        if (array_key_exists('street', $orderResponse['debtor_company'])) {
+            // newer version in shipOrder
+            $companyAddress->street = $orderResponse['debtor_company']['street'];
+            $companyAddress->houseNumber = $orderResponse['debtor_company']['house_number'];
+            $companyAddress->city = $orderResponse['debtor_company']['city'];
+            $companyAddress->postalCode = $orderResponse['debtor_company']['postal_code'];
+            $companyAddress->countryCode = $orderResponse['debtor_company']['country'];
+        } else {
+            $companyAddress->street = $orderResponse['debtor_company']['address_street'];
+            $companyAddress->houseNumber = $orderResponse['debtor_company']['address_house_number'];
+            $companyAddress->city = $orderResponse['debtor_company']['address_city'];
+            $companyAddress->postalCode = $orderResponse['debtor_company']['address_postal_code'];
+            $companyAddress->countryCode = $orderResponse['debtor_company']['address_country'];
+        }
+
         $object->debtorCompany = new Company('', $orderResponse['debtor_company']['name'], $companyAddress);
 
         if ($orderResponse['invoice']) {
             $invoice = new Invoice();
-            $invoice->number = $orderResponse['invoice']['invoice_number'];
+
+            if (array_key_exists('number', $orderResponse['invoice'])) {
+                $invoice->number = $orderResponse['invoice']['number'];
+            } else {
+                $invoice->number = $orderResponse['invoice']['invoice_number'];
+            }
+
             $invoice->payoutAmount = $orderResponse['invoice']['payout_amount'];
             $invoice->feeAmount = $orderResponse['invoice']['fee_amount'];
             $invoice->feeRate = $orderResponse['invoice']['fee_rate'];
